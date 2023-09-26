ca0b6f9bcca759d1e1e422bf3f882309a6b1db84
cna karimi <karimicna@gmail.com>
2023-09-26 13:19:00 +0330

    this is the second one

---
+++ chapter 3/3_1_names.py
@@ -0,0 +1,6 @@
+my_friends = ["danyal", "mostafa", "mahdi", "ali"]
+
+print(my_friends[0])
+print(my_friends[1])
+print(my_friends[2])
+print(my_friends[3])
---
+++ chapter 3/3_2_greetings.py
@@ -0,0 +1,6 @@
+my_friends = ["danyal", "mostafa", "mahdi", "ali"]
+
+print(f'{my_friends[0].title()} welcome to my house')
+print(f'{my_friends[1].title()} welcome to my house')
+print(f'{my_friends[2].title()} welcome to my house')
+print(f'{my_friends[3].title()} welcome to my house')
